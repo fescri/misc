9c91028b380b34a79c724f7d20e02443c011b127
Francisco Escribano <francisco.escribano@bbva.com>
2015-12-01 11:50:36 +0100

    añadido Pom y clase Main

---
+++ src/main/java/hello/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * Created by franciscoescribano on 1/12/15.
+ */
+public class HelloWorldConfiguration {
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
