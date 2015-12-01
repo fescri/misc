e804ae074b5f3b142c46a7d8cbc1dcbf07c10a3d
Francisco Escribano <francisco.escribano@bbva.com>
2015-12-01 12:56:36 +0100

    SpringApplication.run()

--- src/main/java/hello/HelloWorldConfiguration.java
+++ src/main/java/hello/HelloWorldConfiguration.java
@@ -1,12 +1,16 @@
 package hello;
 
-/**
- * Created by franciscoescribano on 1/12/15.
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HelloWorldConfiguration {
 
     public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorldConfiguration.class, args);
         
+
     }
 
 
